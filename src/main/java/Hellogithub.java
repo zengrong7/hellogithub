b53d2c7f42db2b497ad530563cafdd0ecf9c9b97
zengrong7 <1029788275@qq.com>
2020-05-26 10:52:55 +0800

    上传文件

--- src/main/java/Hellogithub.java
+++ src/main/java/Hellogithub.java
@@ -3,5 +3,6 @@ package main.java;
 public class Hellogithub {
     public static void main(String[] args) {
         System.out.println("hello github");
+        System.out.println("上传成功");
     }
 }
